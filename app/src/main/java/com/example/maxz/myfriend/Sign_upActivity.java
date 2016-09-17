fa1e4277f92b69ac6b35b0835f87cca42f17c5bf
Onasume <filmzaza23@gmail.com>
2016-09-17 14:10:55 +0700

    ประกาศค่าตัวแปร

--- app/src/main/java/com/example/maxz/myfriend/Sign_upActivity.java
+++ app/src/main/java/com/example/maxz/myfriend/Sign_upActivity.java
@@ -2,12 +2,48 @@ package com.example.maxz.myfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.VectorEnabledTintResources;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Sign_upActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText,
+            passwordEditText, rePasswordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton maleRadioButton, femaleRadioButton;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget ประกาศค่า ตัวแปร private
+        nameEditText = (EditText) findViewById(R.id.editText);
+        nameEditText = (EditText) findViewById(R.id.editText2);
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        nameEditText = (EditText) findViewById(R.id.editText4);
+        radioGroup = (RadioGroup) findViewById(R.id.ragsex);
+        maleRadioButton = (RadioButton) findViewById(R.id.radioButton);
+        femaleRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
+
+
+
+
     }//main method 2
+
+    public void clickSignupSign(View view) {
+
+
+    } // clickSignup
+
 }//main class 2
