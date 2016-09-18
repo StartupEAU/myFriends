28bb671a62c5c7f7c78d5bba31da693b5896c516
Onasume <filmzaza23@gmail.com>
2016-09-18 15:04:52 +0700

    project finish

--- app/src/main/java/com/example/maxz/myfriend/MainActivity.java
+++ app/src/main/java/com/example/maxz/myfriend/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.maxz.myfriend;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 
 public class MainActivity extends AppCompatActivity {
 
     //ประกาศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private String userString , passwordString;
+
+
 
 
     @Override
@@ -16,9 +29,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //bind Wiget
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
 
 
     }//main method
+
+    public  void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //checkspace
+        if (userString.equals("") || passwordString.equals("")) {
+            myAlert myAlert = new myAlert(this, R.drawable.rat48, "มีช่องว่าง", " thank you");
+            myAlert.myDialog();
+        } else {
+            SynUser synUser = new SynUser();
+            synUser.execute();
+        }
+
+
+
+    } // clicksignin
+
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+        private static final String urlJSON = "http://swiftcodingthai.com/18Sep/get_user.php";
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("MyFriendV3", "JSON ==>" + s);
+        }
+    }// main class
 
     public void click_signUPMain(View view) {
 
--- app/src/main/java/com/example/maxz/myfriend/Sign_upActivity.java
+++ app/src/main/java/com/example/maxz/myfriend/Sign_upActivity.java
@@ -1,10 +1,12 @@
 package com.example.maxz.myfriend;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +17,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 import org.jibble.simpleftp.SimpleFTP;
 
@@ -175,8 +184,64 @@ public class Sign_upActivity extends AppCompatActivity {
         Log.d("MYFriendV2", "Image" + imageNameString);
 
 
+        MyUpdateUser myUpdateUser = new MyUpdateUser(this);
+        myUpdateUser.execute();
+
 
     } // insertData
+
+    private class MyUpdateUser extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private static final String urlPHP = "http://swiftcodingthai.com/18Sep/add_film.php";
+
+        public MyUpdateUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("MyFriendV2", "Result ==>" + s);
+
+            if (Boolean.parseBoolean(s)) {
+                Toast.makeText(context, "อัพข้อมูลเรียบร้อยแล้ว", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()// การโยนภายในครั้งเดียว
+                        .add("isAdd", "true")
+                        .add("Name", nameString)
+                        .add("Sex", sexString)
+                        .add("User", userString)
+                        .add("Password", passwordString)
+                        .add("Image", imageNameString)
+                        .build();
+
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlPHP).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                return null;
+            }
+
+        } // do in back
+    } // Myupdateuserclass
+
 
     private void uploadImageToServer() {
 
@@ -199,6 +264,7 @@ public class Sign_upActivity extends AppCompatActivity {
             Log.d("MyFriendV1", "upload Finish");
 
         } catch (Exception e) {
+            e.printStackTrace();
             Log.d("MyFriendV1", "e ==>" + e.toString());
         }
     }
